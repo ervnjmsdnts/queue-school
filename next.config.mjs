ccd461f53affde907a7dba7c688b9ec4eea65d56
ervnjmsdnts <earvinjamesdantes@gmail.com>
2024-10-11 06:56:26 +0800

    removed webpack

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: function (config, { isServer }) {
-    // Your other webpack configs
-
-    config.experiments = { ...config.experiments, asyncWebAssembly: true }
-
-    return config
-  }
-};
+const nextConfig = {};
 
 export default nextConfig;
